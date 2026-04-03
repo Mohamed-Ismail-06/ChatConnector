8f7063dc2405ba14263de43f3f8a68618cae407e
KRATOS-06 <mohamedmi5520401@gmail.com>
2026-04-03 09:45:34 +0530

    project setup completed

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Router, Routes } from "react-router-dom"
+
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route>
+                    
+                </Route>
+            </Routes>
+        </Router>
+    )
+}
+
+export default App
